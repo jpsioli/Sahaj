7c2db2b2b9a1b24b98950946249a924653dcc33c
João Paulo <jpsioli2010@gmail.com>
2022-10-05 01:34:03 -0300

    feat: menu mobile worked!

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $("#navbar a, .btn").on("click", function (event) {
         event.preventDefault();
 
         const hash = this.hash;
+        nav.classList.toggle("active");
 
         $("html, body").animate(
             {
@@ -15,10 +16,74 @@ $("#navbar a, .btn").on("click", function (event) {
 });
 
 // Sticky menu background
-window.addEventListener("scroll", function () {
-    if (window.scrollY > 150) {
-        document.querySelector("#navbar").style.opacity = 0.9;
-    } else {
-        document.querySelector("#navbar").style.opacity = 1;
-    }
-});
+//  window.addEventListener("scroll", function () {
+//      if (window.scrollY > 150) {
+//          document.querySelector("#navbar").style.opacity = 0.99;
+//      } else {
+//          document.querySelector("#navbar").style.opacity = 1;
+//      }
+//  });
+
+
+       window.onscroll = function() {scrollFunction()};
+
+       function scrollFunction() {
+       if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+           document.getElementById("navbar").style.padding = "30px 15%";
+           document.getElementById("logo").style.height = "80px";
+           document.getElementById("navbar-items").style.top = "4.7875rem";
+       } else {
+           document.getElementById("navbar").style.padding = "70px 15%";
+           document.getElementById("logo").style.height = "130px";
+           document.getElementById("navbar-items").style.top = "8.5875rem";
+       }
+       }
+
+
+       //Menu Mobile
+    
+       const nav = document.querySelector("#navbar");
+       const btnMenu = document.querySelector(".btn-menu");
+       const menu = document.querySelector("#navbar-items");
+       
+       function handleButtonClick(event){
+           if(event.type === "touchstart") event.preventDefault();
+           event.stopPropagation();
+           nav.classList.toggle("active");
+           handleClickOutside(menu, () => {
+               nav.classList.remove("active");
+               setAria();
+           });
+           setAria();
+       }
+       
+       function handleClickOutside(targetElement, callback){
+           const html = document.documentElement;
+           function handleHTMLClick(event){
+               if(!targetElement.contains(event.target)){
+                   targetElement.removeAttribute("data-target");
+                   html.removeEventListener("click", handleHTMLClick);
+                   html.removeEventListener("touchstart", handleHTMLClick);
+                   callback();
+               }
+           }
+           if(!targetElement.hasAttribute("data-target")){
+               html.addEventListener("click", handleHTMLClick);
+               html.addEventListener("touchstart", handleHTMLClick);
+               targetElement.setAttribute("data-target", "");
+           }
+       }
+       
+       function setAria(){
+           const isActive = nav.classList.contains("active");
+           btnMenu.setAttribute("aria-expanded", isActive);
+           if(isActive){
+               btnMenu.setAttribute("aria-label", "Fechar Menu");
+           }else{
+               btnMenu.setAttribute("aria-label", "Abrir Menu");
+           }
+       
+       }
+       
+       btnMenu.addEventListener("click", handleButtonClick);
+       btnMenu.addEventListener("touchstart", handleButtonClick);
